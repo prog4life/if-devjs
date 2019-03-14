80e48f1f3793adb551baf1a17aef8f68d9348200
prog4love <prog4love@users.noreply.github.com>
2019-03-14 19:27:36 +0200

    Update dependencies, better page header layout

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Divider } from 'antd';
-
 const PageHeader = ({ text }) => (
   <header className="page-header">
-    <div>
-      <span className="page-header__text">
+    <div className="page-header__title">
+      <span>
         {text}
       </span>
     </div>
-    {/* <div className="header__middle-line" /> */}
-    <Divider />
+    <hr className="page-header__line" />
   </header>
 );
 
